9a162c748028b9eaa893a5e40a90cfc7ed191b3d
Rob Francken <robertfrancken@gmail.com>
2024-04-28 18:52:14 +1000

    Merge billing controllers

--- app/Http/Controllers/Billing/BillController.php
+++ app/Http/Controllers/Billing/BillController.php
@@ -6,11 +6,30 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Billing\BillStoreRequest;
 use App\Http\Resources\Billing\BillResource;
 use App\Models\Bill;
+use App\Models\User;
+use App\Services\UserBillingService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpFoundation\Response;
 
 class BillController extends Controller
 {
+    protected UserBillingService $userBillingService;
+
+    public function __construct()
+    {
+        $this->userBillingService = UserBillingService::new();
+    }
+
+    public function index(): JsonResponse
+    {
+        Gate::authorize('viewAny', User::class);
+
+        $users = $this->userBillingService->getUserBillingQuery()->get();
+
+        return response()->json($users);
+    }
+
     public function store(BillStoreRequest $request): JsonResponse
     {
         $bill = Bill::create($request->validated());
