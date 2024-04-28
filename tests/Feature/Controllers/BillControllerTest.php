db04a5acb10e58b81ca4504a53c9c0d9711149f4
Rob Francken <robertfrancken@gmail.com>
2024-04-28 18:53:42 +1000

    Merge billing tests

--- tests/Feature/Controllers/BillControllerTest.php
+++ tests/Feature/Controllers/BillControllerTest.php
@@ -5,12 +5,37 @@ namespace Tests\Feature\Controllers;
 use App\Http\Resources\Billing\BillResource;
 use App\Models\Bill;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BillControllerTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_guests_cannot_access_the_user_billing_index_endpoint(): void
+    {
+        $this->getJson(route('bills.index'))
+            ->assertForbidden();
+    }
+
+    public function test_user_billing_index_endpoint_returns_the_expected_json_payload(): void
+    {
+        Bill::factory()->count(3)->submitted()->assignedToUser()->create();
+        Bill::factory()->count(2)->submitted()->assignedToUser()->create();
+        Bill::factory()->count(1)->approved()->assignedToUser()->create();
+
+        $this
+            ->actingAsAdmin()
+            ->getJson(route('bills.index'))
+            ->assertOk()
+            ->assertJsonStructure([
+                '*' => [
+                    'name',
+                    'total_bills',
+                    'total_submitted_bills',
+                    'total_approved_bills',
+                ],
+            ]);
+    }
 
     public function test_guests_cannot_create_bills(): void
     {
