49ba7e598890ea9abcdb1d2318fac55ce9155ce4
Nathaniel Rogers <nathaniel.s.rogers@gmail.com>
2024-07-29 11:38:42 +1000

    Draft bill creation - user assignment is in console command

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -21,45 +21,21 @@ class BillController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * Create a new bill - not assigned to any user in Draft stage
      */
     public function store(Request $request)
     {
         $request->validate([
-            'user_id' => ['bail', 'required', 'exists:users,id'],
-            'date' => 'required|date',
+            'reference' => 'required|max:255',
         ]);
 
-        $user = User::find($request->user_id);
-        // obsoleted by exists validation rule
-//        if (!$user) {
-//            // Missing entity with ID could be either 400, 404, 412 or 422 and I'd use precadent from other code here
-//            return response()->json([
-//                'error' => 'No user found with id ['.$request->user_id.']',
-//                'result' => 'fail'
-//            ], 422);
-//
-//        }
-
-        $bill_stage = BillStage::where('label', 'Submitted')->first();
-        if (!$bill_stage) {
-            // Missing bill stage - indicates migrations haven't run
-            return response()->json([
-                'error' => 'Missing billing stage',
-                'result' => 'fail'
-            ], 500);
-        }
-
+        $bill_stage = BillStage::where('label', 'Draft')->first();
         // Create the bill
         $bill = Bill::create([
-            'bill_reference' => $user->name,
-            'bill_date' => $request->date,
-            'submitted_at' => \Illuminate\Support\Carbon::now()->format('Y-m-d H:i:s'),
+            'bill_reference' => $request->reference,
+            'bill_date' => Carbon::now(),
             'bill_stage_id' => $bill_stage->id
         ]);
-
-        // Add it to the user - obsoleted. This should be done by the new command
-//        $user->bills()->attach($bill);
-
 
         return response()->json([
             'bill_id' => $bill->id,
